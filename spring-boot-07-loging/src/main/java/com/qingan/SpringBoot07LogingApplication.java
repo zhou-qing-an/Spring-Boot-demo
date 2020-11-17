99f25d0bd927ca4b50ba98251ad1032813c467d9
zhou-qing-an <308833854@qq.com>
2020-11-17 10:11:17 +0800

    日志信息

---
+++ spring-boot-07-loging/src/main/java/com/qingan/SpringBoot07LogingApplication.java
@@ -0,0 +1,13 @@
+package com.qingan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot07LogingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot07LogingApplication.class, args);
+    }
+
+}
